c4ec2d214adadfb20012fbb993cebe999804b4b3
tinarazic <tina.razic@gmail.com>
2017-05-03 08:44:09 +0200

    Add files via upload

---
+++ uvoz/uvoz_dolzina.r
@@ -0,0 +1,24 @@
+uvoz.dolzina <- function(dolzine){
+
+# uvoz podatkov iz csv datoteke, dolzina zakonske zveze:
+  
+library(readr)
+library(dplyr)
+library(reshape2)
+
+dolzina <- read_csv("podatki/dolzina_zakonske_zveze.csv",
+                    locale=locale(encoding="windows-1250"),
+                    skip = 1,
+                    col_names = c("leto", "drzava", "trajanje", "izbrisi1", "stevilo", "izbrisi2"),
+                    na=c("", " ", ":"))
+
+dolzina$izbrisi1 <- NULL
+dolzina$izbrisi2 <- NULL
+
+dolzina <- dolzina[!(is.na(dolzina$stevilo)),]
+
+dolzina$leto <- parse_integer(dolzina$leto)
+
+return(dolzina)
+}
+
---
+++ uvoz/uvoz_stevilo.r
@@ -0,0 +1,22 @@
+uvoz.stevilo <-function(zakoni){
+# uvoz podatkov iz csv datoteke, stevilo zakonskih zvez:
+  
+library(readr)
+library(dplyr)
+library(reshape2)
+
+poroke <- read_csv("podatki/stevilo_zakonov.csv",
+                    locale=locale(encoding="windows-1250"),
+                    skip = 1,
+                    col_names = c("leto", "drzava","izbrisi1", "stevilo_porok", "izbrisi2"),
+                    na=c("", " ", ":"))
+
+poroke$izbrisi1 <- NULL
+poroke$izbrisi2 <- NULL
+
+poroke <- poroke[!(is.na(poroke$stevilo_porok)),]
+
+poroke$leto <- parse_integer(poroke$leto)
+
+return(poroke)
+}
