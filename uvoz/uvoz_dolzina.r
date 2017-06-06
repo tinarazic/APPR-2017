d5a7eceba5cf99d9a3161ce5d643b587de5471ac
Tina R <tina.razic@gmail.com>
2017-06-06 22:03:39 +0200

    popravljen projekt

--- uvoz/uvoz_dolzina.r
+++ uvoz/uvoz_dolzina.r
@@ -2,19 +2,22 @@
   
 library(readr)
 library(dplyr)
-library(reshape)
+library(reshape2)
+library(tidyr)
 
 dolzina <- read_csv("podatki/dolzina_zakonske_zveze.csv",
-                    locale=locale(encoding="windows-1250"),
+                    locale = locale(encoding = "windows-1250"),
                     skip = 1,
                     col_names = c("leto", "drzava", "trajanje", "izbrisi1", "stevilo", "izbrisi2"),
-                    na=c("", " ", ":"))
+                    col_types = cols(trajanje = col_factor(c("Less than 1 year",
+                                                             "Less than 5 years",
+                                                             "10 years",
+                                                             "From 15 to 19 years",
+                                                             "From 20 to 24 years",
+                                                             "25 years or over"),
+                                                           ordered = TRUE)),
+                    na=c("", " ", ":")) %>% select(-izbrisi1, -izbrisi2) %>% drop_na()
 
-dolzina$izbrisi1 <- NULL
-dolzina$izbrisi2 <- NULL
-
-dolzina <- dolzina[!(is.na(dolzina$stevilo)),]
-
-dolzina$leto <- parse_integer(dolzina$leto)
 dolzina$drzava<- gsub("^Germany.*$","Germany",dolzina$drzava)
 dolzina$drzava <- gsub("^Former.*$","Macedonia",dolzina$drzava)
+
