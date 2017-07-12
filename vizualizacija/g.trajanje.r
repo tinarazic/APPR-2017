ed332b88e3533e9c794dcba4d1f85e765d093816
Tina R <tina.razic@gmail.com>
2017-07-13 01:48:59 +0200

    popravek

--- vizualizacija/g.trajanje.r
+++ vizualizacija/g.trajanje.r
@@ -4,7 +4,7 @@ library(nlme)
 
 g.trajanje <- ggplot(dolzina %>% 
                        filter(leto > 2014) %>% 
-                       filter(trajanje != "Od 20 do 24 let" & trajanje != "Manj kot eno leto"))+
+                       filter(trajanje != "Od 20 do 24 let" & trajanje != "Manj kot 1 leto"))+
   aes(x = drzava, y = stevilo, color = trajanje) + geom_point() +
   theme(axis.text.x = element_text(angle = 65, hjust = 1)) + 
   xlab("DRŽAVA") + ylab("ŠTEVILO LOČITEV") +
